3c663408d80960d6de376e20c7db62a9277f0b08
adrieldias <adrieldias@hotmail.com>
2015-11-25 18:49:51 -0200

    Criação do model Pedido. Signed-off-by: adrieldias <adrieldias@hotmail.com>

---
+++ app/models/pedido.js
@@ -0,0 +1,29 @@
+// app/models/pedido.js
+var mongoose = require('mongoose');
+
+module.exports = function(){
+	var schema = mongoose.Schema({
+		cod_pedido_cab: { type: Number, required: true },
+		status: {type: String, required: true},
+		prazoVencimento: {
+			type: mongoose.Schema.ObjectId,
+			ref: 'PrazoVencimento',
+			required: true
+		},
+		observacao: {type: String},
+		tipoFrete: {type: String},
+		tranportador: {
+			type: mongoose.Schema.ObjectId,
+			ref: 'Transportador'
+		},
+		ordemCompra: {
+			type: Number				
+		},
+		dataEmissao: {
+			type: Date,
+			required: true	
+		}
+	});
+	
+	return mongoose.model('Pedido', schema);
+};
